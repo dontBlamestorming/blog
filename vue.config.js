ab29abe7c2a5480cae93e296ca0f92e16ee552e3
ukdong <lklone20005@gmail.com>
2021-10-20 00:17:03 +0900

    vue @ -> /src 추가 및 devServer proxy config(cors)

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+const path = require("path");
+
 module.exports = {
   transpileDependencies: ["vuetify"],
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@", path.resolve(__dirname, "src/"));
+  },
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       '@': path.join(__dirname, 'src/')
+  //     }
+  //   }
+  // }
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:8000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 };
