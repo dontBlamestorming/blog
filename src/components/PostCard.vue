91836664f9d7c5019d2dc3bf988f1ed6fc9e066d
ukdong <lklone20005@gmail.com>
2021-10-20 10:53:28 +0900

    add post list view && post view

---
+++ src/components/PostCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <v-img :src="post.thumbnail_url"></v-img>
+    <v-card-title>{{ post.title }}</v-card-title>
+    <v-card-subtitle>{{ post.sub_title }}</v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    post: {
+      /*
+        id: Int,
+        title: String,
+        sub_title: String,
+        content: String - md,
+        thumbnail_url: String - url,
+        post_data: String - yyyy.mm.dd,
+     */
+      type: Object,
+      default: () => ({}), // 만약 비동기 에러난다면 default에서 기본값 처리
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/PostListView.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="(post, index) in postList"
+      :key="index"
+      cols="auto"
+      sm="6"
+      md="6"
+      lg="4"
+      xl="3"
+    >
+      <post-card :post="post"></post-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import PostCard from "../components/PostCard";
+
+export default {
+  name: "PostListView",
+  components: {
+    PostCard,
+  },
+  computed: {
+    ...mapGetters({
+      postList: "getPostList",
+    }),
+  },
+  created() {
+    this.$store.dispatch("fetchPostList");
+  },
+};
+</script>
+
+<style scoped></style>
