76349e2d1375cc98bcb64a97487d6b385c154105
ukdong <lklone20005@gmail.com>
2021-10-25 21:53:02 +0900

    add fetch in app component

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,15 @@
 <script>
 export default {
   name: "App",
+  created() {
+    this.$store.dispatch("fetchPostList");
+  },
 };
 </script>
+
+<!--<v-progress-circular-->
+<!--  :size="50"-->
+<!--  :width="7"-->
+<!--  color="primary"-->
+<!--  indeterminate-->
+<!--&gt;</v-progress-circular>-->
