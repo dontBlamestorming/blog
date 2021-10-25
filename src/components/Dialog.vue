ff1ad27ddafb5d6c9631847c0154c491258fcca1
ukdong <lklone20005@gmail.com>
2021-10-25 21:52:14 +0900

    add Dialog component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog
+    v-model="showDialog"
+    width="500"
+    v-click-outside="onClickOutSide"
+  ></v-dialog>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dialog",
+  methods: {
+    onClickOutSide() {
+      this.$store.commit("SET_DIALOG");
+    },
+  },
+  computed: {
+    ...mapGetters({
+      showDialog: "getShowDialog",
+    }),
+  },
+};
+</script>
+
+<style scoped></style>
