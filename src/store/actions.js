64dfd8cdfd44ff4d92548c30633a71a3f78f4afb
ukdong <lklone20005@gmail.com>
2021-10-25 22:01:41 +0900

    add fetchPost, setPostId, changeCurrentCategoryId Actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,14 +1,32 @@
 import axios from "axios";
 
+axios.defaults.baseURL = "/";
+
 export default {
   fetchPostList({ commit }) {
     axios
-      .get("api/test/posts")
+      .get("api/category/")
       .then((response) => {
         commit("SET_POST_LIST", response.data);
       })
       .catch((error) => {
         console.error(error);
       });
+  },
+  fetchPost({ commit }, postId) {
+    axios
+      .get(`api/posts/${parseInt(postId)}`)
+      .then((response) => {
+        commit("SET_POST", response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  setPostId({ commit }, id) {
+    commit("SET_POST_ID", id);
+  },
+  changeCurrentCategoryId({ commit }, id) {
+    commit("SET_CATEGORY_ID", id);
   },
 };
